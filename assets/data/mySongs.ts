75d0801eb8d135451e8da9dca11ff8ae9181ce89
KennithYu <52869485+KennithYu@users.noreply.github.com>
2019-07-24 23:16:14 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import { songs } from '../../app/songs';
+
+export const MYSONGS: songs[] = [
+    {songname: 'Snapping', artist: 'Chungha', genre: 'Pop', year: 2019, picture: 'snapping.jpg'},
+    {songname: 'Reach for the Summit', artist: 'Lena Raine', genre: 'Electronic', year: 2018, picture: 'celeste.jpg'},
+    {songname: 'Bad Boy', artist: 'Red Velvet', genre: 'Pop', year: 2018, picture: 'badboy.jpg'},
+    {songname: 'Gotta Go', artist: 'Chungha', genre: 'Pop', year: 2018, picture: 'gottago.jpg'}
+]
